b0eaa5a22701a3e07dcdee0a160d29348afd7b8b
TuRan-sino <13275817339@163.com>
2021-08-13 17:17:35 +0800

    改名部分示范代码, 添加了队列代码

---
+++ Datastructure/Repositories/Demostration/NodeQueue.c
@@ -0,0 +1,7 @@
+/*
+	链式存储的队列
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
--- Datastructure/Repositories/Demostration/NodeStack.c
+++ Datastructure/Repositories/Demostration/NodeStack.c
@@ -1,5 +1,9 @@
 /*
 	带头节点栈的链式表示
+	规定只能再链头(头节点之后的那个节点)插入或者删除
+		使用头插法建立单链表的方式来进栈,
+		再表头的位置删除来出栈;
+	一般情况下, 使用不带头节点方式建立代码 	
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,10 +22,9 @@ typedef struct StackNode{
 
 bool InitList(LinkList *S);
 bool ShowList(LinkList S);
-bool Creat_List_Head(LinkList S);
 bool GetTop(LinkList S, ElemType *e);
-bool pop(LinkList S, ElemType *e);
-bool push(LinkList S, ElemType e);
+bool pop(LinkList *S, ElemType *e);
+bool push(LinkList *S);
 bool DestoryList(LinkList S);
 
 
@@ -30,22 +33,21 @@ int main(int argc, char const *argv[])
 	LinkList S;
 	InitList(&S);
 
-	printf("Please Enter your Stack\n");
-	Creat_List_Head(S);
+	printf("Please enter your number\n");
+	push(&S);
 
-	push(S, 3);
-	int e;
-	pop(S, &e);
+	int e = 0;
+	pop(&S, &e);
+	pop(&S, &e);
 
 
 
 	ShowList(S);
 
+	printf("出栈元素: ");
 	printf("%d\n", e);
 
-
 	DestoryList(S);
-	return 0;
 }
 
 bool InitList(LinkList *S)
@@ -59,21 +61,6 @@ bool InitList(LinkList *S)
 	return TRUE;
 }
 
-bool Creat_List_Head(LinkList S)
-{
-	StackNode *p = (StackNode *) malloc(sizeof(StackNode));
-	int x = 0;
-	scanf("%d", &x);
-	while(x != -9999){
-		p->data = x;
-		p->next = S->next;
-		S->next = p;
-		scanf("%d", &x);
-		p = (StackNode *) malloc(sizeof(StackNode));
-	}
-
-	return TRUE;
-}
 
 bool ShowList(LinkList S)
 {
@@ -117,26 +104,32 @@ bool DestoryList(LinkList S)
 	return TRUE;
 }
 
-bool push(LinkList S, ElemType e)
+bool push(LinkList *S)
 {
-	StackNode *p = (StackNode *) malloc(sizeof(StackNode));
-	if(p == NULL){
-		return FALSE;
+	int e = 0;
+	scanf("%d", &e);
+	while(e != -9999){
+		StackNode *p = (StackNode *) malloc(sizeof(StackNode));
+		if(p == NULL){
+			return FALSE;
+		}
+		p->data = e;
+		p->next = (*S)->next;
+		(*S)->next = p;
+		scanf("%d", &e);
 	}
-	p->data = e;
-	p->next = S->next;
-	S->next = p;
+	
 
 	return TRUE;
 }
 
-bool pop(LinkList S, ElemType *e)
+bool pop(LinkList *S, ElemType *e)
 {
-	if(S->next == NULL){
+	if((*S)->next == NULL){
 		return FALSE;
 	}
-	StackNode *p = S->next;
-	S->next = p->next;
+	StackNode *p = (*S)->next;
+	(*S)->next = p->next;
 	*e = p->data;
 	free(p);
 
