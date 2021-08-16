e5080393a627e79ea45d9246f817d52b97b42c97
TuRan-sino <13275817339@163.com>
2021-08-16 19:51:16 +0800

    编辑链式存储的队列相关代码

---
+++ Datastructure/Repositories/Demostration/NodeQueue_NoHead.c
@@ -0,0 +1,103 @@
+/*
+	链式存储的队列
+	不带头结点的版本
+		front指向第一个元素
+		rear指向最后一个元素	
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int ElemType;
+typedef struct LinkNode{	// 链式结点
+	ElemType data;
+	struct LinkNode *next;
+}LinkNode;
+typedef struct{				// 队列结点
+	LinkNode *front;
+	LinkNode *rear;
+}LinkQueue;
+
+bool InitQueue(LinkQueue *Q);
+bool IfEmpty(LinkQueue Q);
+bool EnQueue(LinkQueue *Q, ElemType x);
+bool OutQueue(LinkQueue *Q, ElemType *x);
+bool ShowQueue(LinkQueue Q);
+
+int main(int argc, char const *argv[])
+{
+	LinkQueue Q;
+	InitQueue(&Q);
+
+	EnQueue(&Q, 3);
+
+	ShowQueue(Q);
+
+
+	return 0;
+}
+
+bool InitQueue(LinkQueue *Q)
+{	
+	Q->front = NULL;
+	Q->rear = NULL;	
+
+	return TRUE;
+}
+
+bool IfEmpty(LinkQueue Q)
+{
+	if(Q.front == NULL)		// 也可以判断rear是否指向null
+		return TRUE;
+	else 
+		return FALSE;
+}
+
+bool EnQueue(LinkQueue *Q, ElemType x)
+{
+	LinkNode *s = (LinkNode *) malloc(sizeof(LinkNode));
+	s->data = x;
+	s->next = NULL;
+	if(Q->front == NULL){	// 链队列为空的情况下	
+		Q->front = s;
+		Q->rear = s;
+	}else{					// 链队列不为空的情况下
+		Q->rear->next = s;
+		Q->rear = s;
+	}
+
+	return TRUE;
+}
+
+bool OutQueue(LinkQueue *Q, ElemType *x)
+{
+	if(IfEmpty(*Q) == TRUE)
+		return FALSE;
+	LinkNode *temp = Q->front;
+	*x = temp->data;
+	Q->front = temp->next;
+	if(Q->rear == temp){
+		Q->front = NULL;
+		Q->rear = NULL;
+	}
+	free(temp);
+
+	return TRUE;
+}
+
+bool ShowQueue(LinkQueue Q)
+{
+	LinkNode *temp;
+	temp = Q.rear;
+	if(temp->next == NULL)
+		return FALSE;
+	while(temp->next == NULL){
+		temp = temp->next;
+		printf("%d", temp->data);
+	}
+
+	return TRUE;
+}
---
+++ Datastructure/Repositories/Demostration/NodeQueue_WithHead.c
@@ -0,0 +1,108 @@
+/*
+	链式存储的队列
+	带头结点的版本
+		front指向头指针
+		rear指向最后一个元素	
+	入队从rear入;
+	出队从front出;
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int ElemType;
+typedef struct LinkNode{	// 链式结点
+	ElemType data;
+	struct LinkNode *next;
+}LinkNode;
+typedef struct{				// 队列结点
+	LinkNode *front;
+	LinkNode *rear;
+}LinkQueue;
+
+bool InitQueue(LinkQueue *Q);
+bool IfEmpty(LinkQueue Q);
+bool EnQueue(LinkQueue *Q, ElemType x);
+bool ShowQueue(LinkQueue Q);
+bool OutQueue(LinkQueue *Q, ElemType *x);
+
+
+int main(int argc, char const *argv[])
+{
+	LinkQueue Q;
+	InitQueue(&Q);	
+
+	EnQueue(&Q, 3);
+	EnQueue(&Q, 3);
+	EnQueue(&Q, 3);
+	EnQueue(&Q, 3);
+
+	ShowQueue(Q);
+
+	return 0;
+}
+
+bool InitQueue(LinkQueue *Q)
+{	
+	Q->front = Q->rear = (LinkNode *) malloc(sizeof(LinkNode));
+	if(Q->front == NULL || Q->rear == NULL)
+		return FALSE;
+	Q->front->next = NULL;
+
+	return TRUE;
+}
+
+bool IfEmpty(LinkQueue Q)
+{
+	if(Q.front == Q.rear)
+		return TRUE;
+	else
+		return FALSE;	
+}
+
+bool EnQueue(LinkQueue *Q, ElemType x)
+{
+	LinkNode *s = (LinkNode *) malloc(sizeof(LinkNode));
+	if(s == NULL){
+		return FALSE;
+	}
+	s->data = x;
+	s->next = NULL;
+	Q->rear->next = s;
+	Q->rear = s;
+
+	return TRUE;
+}
+
+bool OutQueue(LinkQueue *Q, ElemType *x)
+{
+	if(IfEmpty(*Q) == 1)
+		return FALSE;
+	LinkNode *temp;
+	temp = Q->front->next;
+	*x = Q->front->next->data;
+	Q->front->next = Q->front->next->next;
+	if(Q->rear == temp)
+		Q->rear = Q->front;
+	free(temp);
+
+	return TRUE;
+}
+
+
+bool ShowQueue(LinkQueue Q)
+{
+	LinkNode *temp;
+	temp = Q.front;
+	if(temp->next == NULL)
+		return FALSE;
+	while(temp->next == NULL){
+		temp = temp->next;
+		printf("%d", temp->data);
+	}
+
+	return TRUE;
+}
