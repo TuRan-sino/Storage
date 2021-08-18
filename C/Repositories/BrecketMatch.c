0610860213b16d1f1c9f0e9f5013c0816f421c9b
TuRan <TuRanPROC@gmail.com>
2021-08-18 18:47:49 +0800

    稍微改一下存储位置

---
+++ C/Repositories/BrecketMatch.c
@@ -0,0 +1,29 @@
+/*
+	使用顺序栈实现括号匹配
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define TRUE 1
+#define FALSE 0
+#define MaxSize 20
+
+typedef struct{
+	char data[MaxSize];
+	int top;
+}SqStack;
+
+int main(int argc, char const *argv[])
+{
+		
+
+
+
+	return 0;
+}
+
+bool BracketMatch(char str[], int length)
+{
+	
+}
