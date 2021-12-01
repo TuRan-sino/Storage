aa925032b5609ec56a14b320df6d650e3571323d
TuRan <TuRanPROC@gmail.com>
2021-12-01 20:01:34 +0800

    leet code second question

--- C/Repo/LeedCode/2_AddTwoNumbers.c
+++ C/Repo/LeedCode/2_AddTwoNumbers.c
@@ -1,3 +1,28 @@
+/*  
+给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+
+请你将两个数相加，并以相同形式返回一个表示和的链表。
+
+你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+示例 1：
+输入：l1 = [2,4,3], l2 = [5,6,4]
+输出：[7,0,8]
+解释：342 + 465 = 807.
+示例 2：
+
+输入：l1 = [0], l2 = [0]
+输出：[0]
+示例 3：
+
+输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+输出：[8,9,9,9,0,0,0,1]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/add-two-numbers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -51,9 +76,9 @@ bool CreatList(ListNode *L)
 	printf("Please enter your number\n");
 	scanf("%d", &receiver);
 	L->val = receiver;
-	while(receiver != -9999){
+	while(receiver != -1){
 		scanf("%d", &receiver);
-		if(receiver == -9999){
+		if(receiver == -1){
 			return true;
 		}
 		s = (ListNode *)malloc(sizeof(ListNode));
@@ -87,26 +112,46 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
 	int carry_last = 0;
 	int carry_now = 0;
 	int result = 0;
+	int tag = 0;
 
 	while(l1 != NULL && l2 != NULL){
-		result = l1->val + l2->val;
+		result = l1->val + l2->val + carry_now;
 		if(result >= 10){
+			tag = 1;
 			result = result - 10;
 			carry_last = 1;
 		}else{
 			carry_last = 0;
 		}
-		r->val = carry_now + result;
+		r->val =result;
 		carry_now = carry_last;
 
-		
-
+		if(l1->next == NULL && l2->next == NULL){
+			if(tag == 1 && carry_last == 1){
+				s = (struct ListNode *)malloc(sizeof(struct ListNode));
+				s->val = 1;
+				s->next = NULL;
+				r->next = s;
+				r = s;
+			}
+			break;
+		}else if(l1->next == NULL && l2->next != NULL){
+			l2 = l2->next;
+			l1->val = 0;
+		}else if(l1->next != NULL && l2->next == NULL){
+			l1 = l1->next;
+			l2->val = 0;
+		}else{
+			l1 = l1->next;
+			l2 = l2->next;
+		}
 
 		s = (struct ListNode *)malloc(sizeof(struct ListNode));
 		s->next = NULL;
 		r->next = s;
 		r = s;
 	}
+
 	
 	return ret;
 	
